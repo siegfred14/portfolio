2151cb768ae8b9f76636a0a167cb9ee5444fe121
Siegfred Samson <siegfredsamson@gmail.com>
2022-07-19 12:31:38 +0100

    floating nav styled

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -6,26 +6,37 @@ import { GiSkills, GiSpanner } from "react-icons/gi";
 import { BiBriefcase } from "react-icons/bi";
 import { BsMailbox } from "react-icons/bs";
 
+// const changeNav = () =>{
+//   let navNew = document.querySelector('.nav');
+//   navNew.background
+// }
+
 const Nav = () => {
   return (
     <nav>
-      <a href="#">
+      <a href="#" className="active">
         <FaHome />
+        <span>home</span>
       </a>
       <a href="#about">
         <FiUser />
+        <span>about</span>
       </a>
       <a href="#experience">
         <GiSkills />
+        <span>experience</span>
       </a>
       <a href="#services">
         <GiSpanner />
+        <span>services</span>
       </a>
       <a href="#portfolio">
         <BiBriefcase />
+        <span>portfolio</span>
       </a>
       <a href="#contact">
         <BsMailbox />
+        <span>contact</span>
       </a>
     </nav>
   );
