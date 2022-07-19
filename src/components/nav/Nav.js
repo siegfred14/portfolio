4e2a7bdcd55fbf764a494409655a6e32690bab0d
Siegfred Samson <siegfredsamson@gmail.com>
2022-07-19 12:54:27 +0100

    floating nav active class set

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./nav.css";
 import { FaHome } from "react-icons/fa";
 import { FiUser } from "react-icons/fi";
@@ -12,29 +12,55 @@ import { BsMailbox } from "react-icons/bs";
 // }
 
 const Nav = () => {
+  const [activeNav, setActiveNav] = useState("#");
+
   return (
     <nav>
-      <a href="#" className="active">
+      <a
+        href="#"
+        onClick={() => setActiveNav("#")}
+        className={activeNav === "#" ? "active" : ""}
+      >
         <FaHome />
         <span>home</span>
       </a>
-      <a href="#about">
+      <a
+        href="#about"
+        onClick={() => setActiveNav("#about")}
+        className={activeNav === "#about" ? "active" : ""}
+      >
         <FiUser />
         <span>about</span>
       </a>
-      <a href="#experience">
+      <a
+        href="#experience"
+        onClick={() => setActiveNav("#experience")}
+        className={activeNav === "#experience" ? "active" : ""}
+      >
         <GiSkills />
         <span>experience</span>
       </a>
-      <a href="#services">
+      <a
+        href="#services"
+        onClick={() => setActiveNav("#services")}
+        className={activeNav === "#services" ? "active" : ""}
+      >
         <GiSpanner />
         <span>services</span>
       </a>
-      <a href="#portfolio">
+      <a
+        href="#portfolio"
+        onClick={() => setActiveNav("#portfolio")}
+        className={activeNav === "#portfolio" ? "active" : ""}
+      >
         <BiBriefcase />
         <span>portfolio</span>
       </a>
-      <a href="#contact">
+      <a
+        href="#contact"
+        onClick={() => setActiveNav("#contact")}
+        className={activeNav === "#contact" ? "active" : ""}
+      >
         <BsMailbox />
         <span>contact</span>
       </a>
