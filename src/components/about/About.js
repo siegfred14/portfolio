68254b7abb30d50ffda3116fcbaf59d59fda8800
Siegfred Samson <siegfredsamson@gmail.com>
2022-07-23 23:42:51 +0100

    logic for blur on inactive image

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -8,6 +8,24 @@ import { FiUsers } from "react-icons/fi";
 import { VscFolderLibrary } from "react-icons/vsc";
 
 const About = () => {
+  let collageImages = [...document.querySelectorAll(".collage-img")];
+
+  collageImages.map((item, i) => {
+    item.addEventListener("mouseover", () => {
+      collageImages.map((image, index) => {
+        if (index != i) {
+          image.style.filter = "blur(10px)";
+          item.style.zIndex = 2;
+        }
+      });
+    });
+    item.addEventListener("mouseleave", () => {
+      collageImages.map((image, index) => {
+        image.style = null;
+      });
+    });
+  });
+
   return (
     <section id="about">
       <h5>Get Conversant!</h5>
