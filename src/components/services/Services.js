20a778b30a4fb9967d992fdccb4c00f0e27aac5a
Siegfred Samson <siegfredsamson@gmail.com>
2022-08-06 10:50:38 +0100

    services section > web development section initiated

--- src/components/services/Services.js
+++ src/components/services/Services.js
@@ -1,10 +1,75 @@
 import React from "react";
 import "./services.css";
+import { FaCanadianMapleLeaf } from "react-icons/fa";
 
 const Services = () => {
   return (
     <section id="services">
-      <p>Services section</p>
+      <h5>What I Offer</h5>
+      <h2>My Services</h2>
+
+      <div className="container services_container">
+        {/* web development */}
+        <article className="service">
+          <div className="service_head">
+            <h3>Web Development</h3>
+          </div>
+
+          <ul className="service_list">
+            <li>
+              <FaCanadianMapleLeaf className="service_list_icon" />
+              <p>
+                Writing well designed, testable, efficient code by employing
+                best software development practices
+              </p>
+            </li>
+            <li>
+              <FaCanadianMapleLeaf className="service_list_icon" />
+              <p>
+                Working with product owners to determine business requests,
+                requirements and desired functionality
+              </p>
+            </li>
+            <li>
+              <FaCanadianMapleLeaf className="service_list_icon" />
+              <p>
+                Creating unified layout/user interfaces by using standard
+                HTML/CSS practices
+              </p>
+            </li>
+            <li>
+              <FaCanadianMapleLeaf className="service_list_icon" />
+              <p>
+                Integrating data from various back-end services and databases
+              </p>
+            </li>
+            <li>
+              <FaCanadianMapleLeaf className="service_list_icon" />
+              <p>maintaining, expanding, and scaling of website</p>
+            </li>
+            <li>
+              <FaCanadianMapleLeaf className="service_list_icon" />
+              <p>Contributing to all phases of the development lifecycle</p>
+            </li>
+            <li>
+              <FaCanadianMapleLeaf className="service_list_icon" />
+              <p>
+                Follow best practices such as continuous integration, SCRUM,
+                refactoring and code standards
+              </p>
+            </li>
+            <li>
+              <FaCanadianMapleLeaf className="service_list_icon" />
+              <p>
+                Driving continuous adoption and integration of relevant new
+                technologies into design
+              </p>
+            </li>
+          </ul>
+        </article>
+        {/* photo editing */}
+        {/* sales and public speaking */}
+      </div>
     </section>
   );
 };
