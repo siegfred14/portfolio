b76fe2a2d282fb59187cec89376d9a3d867c91cd
Siegfred Samson <siegfredsamson@gmail.com>
2022-08-08 23:19:49 +0100

    skills section created

--- src/components/skills/Skills.js
+++ src/components/skills/Skills.js
@@ -1,6 +1,17 @@
 import "./skills.css";
 import React from "react";
 import html5 from "../../img/skills/HTML5.png";
+import css3 from "../../img/skills/CSS3.png";
+import sass from "../../img/skills/Sass_Logo.png";
+import js from "../../img/skills/JavaScript-logo.png";
+import python from "../../img/skills/Python_logo.png";
+import django from "../../img/skills/django.png";
+import drf from "../../img/skills/DRF_logo.png";
+import git from "../../img/skills/Git_logo.png";
+import photoshop from "../../img/skills/photoshop.png";
+import bootstrap from "../../img/skills/bootstrap-logo.png";
+import react from "../../img/skills/react.png";
+import mysql from "../../img/skills/mySQL.png";
 
 export const Skills = () => {
   return (
@@ -9,13 +20,81 @@ export const Skills = () => {
       <h2>My Services</h2>
 
       <div className="container skills_container">
-        <div className="skill_box">
+        <div className="skill_box html5">
           <div className="skill_title">
             <div className="img">
               <img src={html5} alt="HTML5" />
             </div>
             <h3>HTML5</h3>
           </div>
+        </div>
+        <div className="skill_box CSS">
+          <div className="skill_title">
+            <div className="img">
+              <img src={css3} alt="CSS3" />
+            </div>
+            <h3>CSS3 | Sass</h3>
+          </div>
+        </div>
+        <div className="skill_box JS">
+          <div className="skill_title">
+            <div className="img">
+              <img src={js} alt="JS" />
+            </div>
+            <h3>JavaScript</h3>
+          </div>
+        </div>
+        <div className="skill_box mysql">
+          <div className="skill_title">
+            <div className="img">
+              <img src={mysql} alt="MySQL" />
+            </div>
+            <h3>MySQL</h3>
+          </div>
+        </div>
+        <div className="skill_box photoshop">
+          <div className="skill_title">
+            <div className="img">
+              <img src={photoshop} alt="Photoshop" />
+            </div>
+            <h3>Photoshop</h3>
+          </div>
+        </div>
+        <div className="skill_box html5">
+          <div className="skill_title">
+            <div className="img">
+              <img src={git} alt="Git" />
+            </div>
+            <h3>Git | Github</h3>
+          </div>
+        </div>
+        <div className="skill_box html5">
+          <div className="skill_title">
+            <div className="img">
+              <img src={react} alt="React" />
+            </div>
+            <h3>React</h3>
+          </div>
+        </div>
+        <div className="skill_box python">
+          <div className="skill_title">
+            <div className="img">
+              <img src={python} alt="Python" />
+            </div>
+            <h3>Python</h3>
+          </div>
+        </div>
+        <div className="skill_box django">
+          <div className="skill_title">
+            <div className="img">
+              <img src={django} alt="Django" />
+            </div>
+            <h3>Django [Rest Framework]</h3>
+          </div>
+        </div>
+
+        <div className="soft_skills">
+          <small>team player</small>
         </div>
       </div>
     </section>
