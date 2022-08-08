a90039e28c5c2b8771fffd2aa855ead9f43f237b
Siegfred Samson <siegfredsamson@gmail.com>
2022-08-08 22:39:52 +0100

    more images for skill section added

--- src/components/skills/Skills.js
+++ src/components/skills/Skills.js
@@ -1,12 +1,23 @@
 import "./skills.css";
-
 import React from "react";
+import html5 from "../../img/skills/HTML5.png";
 
 export const Skills = () => {
   return (
-    <div>
-      <p>Skills Section</p>
-      <p>Hard Skills</p> <p>Soft Skills</p>
-    </div>
+    <section id="skills">
+      <h5>What I Offer</h5>
+      <h2>My Services</h2>
+
+      <div className="container skills_container">
+        <div className="skill_box">
+          <div className="skill_title">
+            <div className="img">
+              <img src={html5} alt="HTML5" />
+            </div>
+            <h3>HTML5</h3>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
