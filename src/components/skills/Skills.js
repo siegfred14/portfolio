88c89f494c104c71bbee52d809f8568f6d884350
Siegfred Samson <siegfredsamson@gmail.com>
2022-08-13 18:25:46 +0100

    skill_icon2 aligned

--- src/components/skills/Skills.js
+++ src/components/skills/Skills.js
@@ -8,6 +8,7 @@ import python from "../../img/skills/Python_logo.png";
 import django from "../../img/skills/django.png";
 import drf from "../../img/skills/DRF_logo.png";
 import git from "../../img/skills/Git_logo.png";
+import github from "../../img/skills/github.png";
 import photoshop from "../../img/skills/photoshop.png";
 import bootstrap from "../../img/skills/bootstrap-logo.png";
 import react from "../../img/skills/react.png";
@@ -28,10 +29,20 @@ export const Skills = () => {
             <h3>HTML5</h3>
           </div>
         </div>
-        <div className="skill_box CSS">
+        <div className="skill_box css_box">
           <div className="skill_title">
             <div className="img">
-              <img src={css3} className="skill_icon" alt="CSS3" />
+              <img src={css3} className="skill_icon css3" alt="CSS3" />
+              <img
+                src={bootstrap}
+                className="skill_icon skill_icon2 bootstrap"
+                alt="css"
+              />
+              <img
+                src={sass}
+                className="skill_icon skill_icon2 sass"
+                alt="sass"
+              />
             </div>
             <h3>CSS3 | Sass</h3>
           </div>
@@ -60,15 +71,20 @@ export const Skills = () => {
             <h3>Photoshop</h3>
           </div>
         </div>
-        <div className="skill_box html5">
+        <div className="skill_box git_box">
           <div className="skill_title">
             <div className="img">
-              <img src={git} className="skill_icon" alt="Git" />
+              <img src={git} className="skill_icon git" alt="Git" />
+              <img
+                src={github}
+                className="skill_icon skill_icon2 github"
+                alt="Github"
+              />
             </div>
             <h3>Git | Github</h3>
           </div>
         </div>
-        <div className="skill_box html5">
+        <div className="skill_box react">
           <div className="skill_title">
             <div className="img">
               <img src={react} className="skill_icon" alt="React" />
@@ -84,10 +100,15 @@ export const Skills = () => {
             <h3>Python</h3>
           </div>
         </div>
-        <div className="skill_box django">
+        <div className="skill_box django_box">
           <div className="skill_title">
             <div className="img">
-              <img src={django} className="skill_icon drf" alt="Django" />
+              <img src={django} className="skill_icon django" alt="Django" />
+              <img
+                src={drf}
+                className="skill_icon skill_icon2 drf"
+                alt="Rest Framework"
+              />
             </div>
             <h3>Django [Rest Framework]</h3>
           </div>
