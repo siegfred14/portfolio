d01c614f88231e3277daf45a5c414570c4355680
Siegfred Samson <siegfredsamson@gmail.com>
2022-08-19 23:52:11 +0100

    testimonial section article added

--- src/components/testimonials/Testimonials.js
+++ src/components/testimonials/Testimonials.js
@@ -3,7 +3,23 @@ import React from "react";
 const Testimonials = () => {
   return (
     <section id="testimonials">
-      <p>Testimonials</p>
+      <h5>Client Review</h5>
+      <h2>Testimonials</h2>
+
+      <div className="container testimonials_container">
+        <article className="testimonial">
+          <div className="client_avatar">
+            <img src="" alt="Avatar One" />
+            <h5 className="client_name">mr Jason</h5>
+            <small className="client_review">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil,
+              reprehenderit ab, ducimus nemo sapiente nam nisi totam accusantium
+              ad, consequuntur suscipit enim cumque repellendus asperiores!
+              Officiis deleniti velit totam accusamus.
+            </small>
+          </div>
+        </article>
+      </div>
     </section>
   );
 };
