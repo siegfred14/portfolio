34b07cd97617b6438cad5f2741e3b4867193ebcf
Siegfred Samson <siegfredsamson@gmail.com>
2022-08-30 23:09:12 +0100

    contact template created

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -16,16 +16,15 @@ const Contact = () => {
 
           <div className="contact_info">
             <div className="contact_card">
-              <i className="bx bx contact_card-icon"></i>
+              <i className="bx bx-mail-send contact_card_icon"></i>
 
               <h3 className="contact_card_title">e-mail</h3>
-              <span className="contact_card_data">
-                siegfredsamson@gmail.com
-              </span>
+              <h5 className="contact_card_data">siegfredsamson@gmail.com</h5>
 
               <a
                 href="mailto:siegfredsamson@gmail.com"
                 className="contact_button"
+                target="_blank"
               >
                 Write Me{" "}
                 <i className="bx bxs-right-arrow-alt bx-sm bx-fade-right-hover contact_button_icon"></i>
@@ -33,24 +32,34 @@ const Contact = () => {
             </div>
 
             <div className="contact_card">
-              <i className="bx bx contact_card-icon"></i>
+              {/* <i className="bx bxl-whatsapp contact_card-icon"></i> */}
+              <IoLogoWhatsapp className="contact_card_icon" />
 
               <h3 className="contact_card_title">Whatsapp</h3>
-              <span className="contact_card_data">+234 803 619 2916</span>
+              <h5 className="contact_card_data">+234 803 619 2916</h5>
 
-              <a href="" className="contact_button">
+              <a
+                href="https://api.whatsapp.com/send?phone=+2348036192916"
+                className="contact_button"
+                target="_blank"
+              >
                 Write Me{" "}
                 <i className="bx bxs-right-arrow-alt bx-sm bx-fade-right-hover contact_button_icon"></i>
               </a>
             </div>
 
             <div className="contact_card">
-              <i className="bx bx contact_card-icon"></i>
+              {/* <i className="bx bxl-messenger contact_card_icon"></i> */}
+              <SiMessenger className="contact_card_icon" />
 
               <h3 className="contact_card_title">Messenger</h3>
-              <span className="contact_card_data">m.me/siegfreds</span>
+              <h5 className="contact_card_data">Siegfred Samson</h5>
 
-              <a href="https://m.me/siegfreds" className="contact_button">
+              <a
+                href="https://m.me/siegfreds"
+                className="contact_button"
+                target="_blank"
+              >
                 Write Me{" "}
                 <i className="bx bxs-right-arrow-alt bx-sm bx-fade-right-hover contact_button_icon"></i>
               </a>
@@ -60,6 +69,55 @@ const Contact = () => {
 
         <div className="contact_content">
           <h3 className="contact_title">Your project</h3>
+
+          <form action="" className="contact_form">
+            <div className="contact_form_div">
+              <label htmlFor="name" className="contact_form_tag">
+                Name
+              </label>
+              <input
+                type="text"
+                name="name"
+                className="contact_form_input"
+                placeholder="Enter your name"
+                required
+              />
+            </div>
+
+            <div className="contact_form_div">
+              <label htmlFor="email" className="contact_form_tag">
+                Email
+              </label>
+              <input
+                type="email"
+                name="name"
+                className="contact_form_input"
+                placeholder="Enter your email"
+                required
+              />
+            </div>
+
+            <div className="contact_form_div">
+              <label htmlFor="project" className="contact_form_tag">
+                Project
+              </label>
+              <textarea
+                name="project"
+                // cols="30"
+                rows="7"
+                type="text"
+                className="contact_form_input"
+                placeholder="Enter your name"
+                required
+              >
+                Write your project
+              </textarea>
+            </div>
+
+            <button type="submit" className="btn btn-primary">
+              Send message
+            </button>
+          </form>
         </div>
       </div>
     </section>
